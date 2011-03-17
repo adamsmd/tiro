9383529575188d92acd480ae0651212fd7d5a433
Michael D. Adams (Computer Science, Indiana University) <devnull@localhost>
2011-03-17 08:11:09 -0400

    Added initial Tiro::Config module

---
+++ Tiro/Config.pm
@@ -0,0 +1,110 @@
+package Tiro::Config;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Tiro::Config - The great new Tiro::Config!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Tiro::Config;
+
+    my $foo = Tiro::Config->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Michael D. Adams, C<< <www.cs.indiana.edu/~adamsmd/> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-tiro-config at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Tiro-Config>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Tiro::Config
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Tiro-Config>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Tiro-Config>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Tiro-Config>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Tiro-Config/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Michael D. Adams.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Tiro::Config
